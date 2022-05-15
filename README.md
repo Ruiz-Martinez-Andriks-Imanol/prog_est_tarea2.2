af883e6a641bd0064b7ff16e8a884fc49fb54fa6
Ruiz-Martinez-Andriks-Imanol <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-15 15:07:42 -0500

    prog_est_tarea2.2
    es un programa que imprime el precio neta de cada ejemplar de novelas y el número de ejemplares que vende.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # prog_est_tarea2.2
-Programa que imprime el precio de neto de cada ejemplar de novelas y el número de ejemplares que venderá.
+<p>Autor: Andriks Imanol Ruiz Mártinez, Fecha: 10/02/2022 <br>
+Escuela: Universidad del Valle de Mexico Campus Villahermosa <br>
+Materia: Programación Estructrada <br>
+Ciclo: 01/2022</p>
+<br>
+<p>Este es un programa en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  <ul>
+    <li>Variables enteras y flotantes</li>
+    <li>printf para mostrar mensajes y variables</li>
+    <li>scanf</li>
+    <li>El uso de include para las librerías</li>
+    <li>Condicionales</li>
+    <li>Espaciado \t y el salto de renglón \n</li>
+    <li>Operadores lógicos</li>
+    <li>Comentarios para la documentación interna del programa</li>
+    </ul>
+    </p>
+<br>
+Objetivo: Calcular el precio neto de los ejemplares de novelas y el número que se venderán.
+<br>
+<br>
+<p>Breve descripción: 
+Es un programa para imprimir el precio neto de cada ejemplar de las novelas y el número de ejemplares que se venderán.
+<br>
+El programa imprime después las regalías en cada opción y la mejor opción que el autor puede escoger. Se deberá usar operadores lógicos como AND (&&) para algunas condicionales.
+<br>
+</p>
---
+++ tarea2.2.cpp
@@ -0,0 +1,50 @@
+/* Autor: Andriks Imanol Ruiz Mártinez, Realizado: 10/02/2022
+Escuela: Universidad del Valle de Mexico Campus Villahermosa
+Materia: Programación Estructrada
+Ciclo: 01/2022
+
+Este es un programa en Lenguaje C de la materia de Programación Estructurada
+Muestra el uso de:
+
+    -Variables enteras y flotantes
+    -printf para mostrar mensajes y variables
+    -scanf
+    -El uso de include para las librerías
+    -Condicionales
+    -Espaciado \t y el salto de renglón \n
+    -Operadores lógicos
+    -Comentarios para la documentación interna del programa
+*/
+#include<stdio.h>
+int main() {
+int ejemplar;
+	//Declaración de variables
+	float opcion, opcion2, opcion3,excedente, valor;
+	//Entrada de datos
+	printf("Introduce precio: ");
+	scanf("%f", &valor);
+	printf("Introduce ejemplares a vender: ");
+	scanf("%d", &ejemplar);
+	//Procedimiento
+		opcion=5000+20000;
+		opcion2=valor*0.125*ejemplar;
+	if(ejemplar>4000){ //Condicionales
+		opcion3=4000*valor*0.10;
+		excedente=(ejemplar-4000)*valor*0.14;
+		opcion3=opcion3+excedente;
+	} else {
+		opcion3=ejemplar*valor*0.10;
+	}
+	//Salida
+printf("\t\t La primera opcion vale:\t-$%10.2f\n", opcion);
+printf("\t\t La segunda opcion  vale:\t-$%10.2f\n", opcion2);
+printf("\t\t La tercera opcion vale:\t-$%10.2f\n", opcion3);
+	if ((opcion>opcion2) && (opcion > opcion3)) {
+	printf("La primera opcion es la mejor ");
+} else if ((opcion2>opcion) && (opcion2 > opcion3)) {
+	printf("La segunda opcion es la mejor ");
+} else {
+	printf("La tercera opcion es la mejor ");
+}
+	return 0;
+}
